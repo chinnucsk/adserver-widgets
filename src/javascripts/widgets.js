58659552f873a0ac7c9c8685b5c745745ed6b95d
nscontext <nscontext@emergent.pl>
2012-07-12 15:16:43 +0200

    slide out have a new require option to defire distance from Top of page to slide out

--- src/javascripts/widgets.js
+++ src/javascripts/widgets.js
@@ -1,38 +1,50 @@
-
 /*
-function init() {
-  alert("test");
-};
 
-$(document).ready(function() { 
-	$('body').append("<div class='adk_fixed' id='adk_fixed'><div class='adk-top'><div class='close'></div><div class='adk-title'>REKLAMA</div></div><div id='adk-adcode'></div></div>");
-};
+  nsContext widgets - ver 0.05
 
-*/
-
+*/ 
 (function($) {  
-	$.fn.nsSlideOut = function(options) {
+	$.fn.ns_slideout = function(options) {
 
 		var defaults = {
 			width: '300px',
 			height: '220px',
-			placement: 'http://adsearch.adkontekst.pl/_/ads0/?QAPS_AKPL=--6347-106877-#10811--&noChache=uvw04y'
+			placement: 'http://adsearch.adkontekst.pl/_/ads0/?QAPS_AKPL=--6347-106877-#10811--&noChache=uvw04y',
+			distanceTop: '900px'
 		};
 		
 		var options = $.extend(defaults, options); 
-
+    
+    // Build widget HTML
 		$('body').append("<div class='adk-fixed' id='adk-fixed'><div class='adk-top'><div class='close'></div><div class='adk-title'>REKLAMA</div></div><div class='adk-adcode'></div></div>");
+
+    var widget = $('.adk-fixed');
 		
-		$('.adk-fixed').height(options['height'])
-			       .width( options['width'] );
+		// Set widget size
+		widget.height( options['height'] ).width( options['width'] );
+			
+	  // Hide widget by default
+		widget.css( {right: "-"+options['width']  } );
 
-		$('.adk-fixed .close').click(function(){ $(".adk-fixed").hide(); });
+    // Remove widget on Close Button click
+		widget.find('.close').click(function(){ $(".adk-fixed").remove(); });
 
+    // Insert Ad Script
 		var script = document.createElement("script"); 
 		script.setAttribute("type", "text/javascript"); 
 		script.setAttribute("src", options["placement"]); 
 		document.getElementsByClassName('adk-adcode')[0].appendChild(script);
 
+    // Show widget on certain page height
+		$(window).scroll(function(){
+      var distanceTop = parseInt( options['distanceTop']) - $(window).height();
+      if(distanceTop < 0 ) distanceTop = parseInt( options['distanceTop']);
+
+      if  ($(window).scrollTop() > distanceTop  )
+        widget.animate({'right': 0 },300);
+      else
+        widget.stop(true).animate({'right':'-'+options['width']},100);
+    });
 		
  		return this;
 	};
