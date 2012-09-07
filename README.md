9e975297c560bf7be4afe8f97f34906d12353ee4
nscontext <nscontext@emergent.pl>
2012-09-07 12:58:26 +0200

    new Slideout parameter: position, let to show placement on left side of websites

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Website widgets
 
 In website HEAD load all require resources: jquery, stylesheets and nscontext-widgets.
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
     <link rel="stylesheet" type="text/css" href="http://www.nscontext.com/widgets/style.css" />
     <script type="text/javascript" src="http://www.nscontext.com/widgets/widgets.js" ></script>
 
@@ -18,15 +18,19 @@ Also in head configure the placement:
 
     <script type="text/javascript">
       //<![CDATA[
+        jQuery.noConflict();
+        (function($) {
         $(document).ready( function() {
           $().ns_slideout({
             width: '300px',
             title: "Ciekawe artykuły z sieci",
             height: '220px',
             placement: 'http://emisja.contentstream.pl/_/ads1/?QAPS_ADART=9468f1f6243d00fb8e5477d6ec2de786',
-            distanceTop: '1600px'
+            distanceTop: '1600px',
+            position: 'right'
           });
         });
+        })(jQuery);
       //]]>
     </script>
 
@@ -47,6 +51,7 @@ Slide Out placement have several parameters that you can use to customize the us
 * title - text that will be shown in placement header (default: "REKLAMY")
 * distanceTop - distance from page top in pixels on which placement will be shown (default: "900px")
 * placement - URL to one of nscontext compatible placement scripts
+* position - right or left side of the website (default: "right")
 
 ## Need help ?
 
--- src/javascripts/widgets.js
+++ src/javascripts/widgets.js
@@ -11,7 +11,8 @@
 			height: '220px',
 			placement: 'http://adsearch.adkontekst.pl/_/ads0/?QAPS_AKPL=--6347-106877-#10811--&noChache=uvw04y',
 			distanceTop: '900px',
-			title: "REKLAMY"
+			title: "REKLAMY",
+      position: "right"
 		};
 		
 		var options = $.extend(defaults, options); 
@@ -23,12 +24,16 @@
     $(".adk-title").html( options["title"] );
 
     var widget = $('.adk-fixed');
+
+    // Set Class name for different positions
+    widget.addClass( "adk-position-"+options["position"] )
+
 		
 		// Set widget size
 		widget.height( options['height'] ).width( options['width'] );
 			
 	  // Hide widget by default
-		widget.css( {right: "-"+options['width']  } );
+		widget.css( options['position'],  "-"+options['width'] );
 
     // Remove widget on Close Button click
 		widget.find('.close').click(function(){ $(".adk-fixed").remove(); });
@@ -38,22 +43,31 @@
 		script.setAttribute("type", "text/javascript"); 
 		script.setAttribute("src", options["placement"]); 
 		$('.adk-adcode')[0].appendChild(script);
+
 		var on_scroll = function() {
       var distanceTop = parseInt( options['distanceTop']);
+
+      var animation = {}
+      animation[ options["position"] ] = 0;
 
       if( $(window).scrollTop() > distanceTop || 
           distanceTop == 0  || 
           $(window).height() === $(document).height() ) {
-        widget.animate({'right': 0 },300);
+        
+        widget.animate( animation ,300);
       } else {
-        widget.stop(true).animate({'right':'-'+options['width']},100);
+
+        animation[ options["position"] ] = "-"+options["width"]; 
+        widget.stop(true).animate( animation ,100);
       }
 		};
+
     // Show widget on certain page height
 		$(window).scroll( on_scroll );
 
 		// Show if page not high enough to have scroll
 		on_scroll()
+
  		return this;
 	};
 })(jQuery);
